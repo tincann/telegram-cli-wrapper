1e4b966a862e99a404d9ae933a7f7db28036a843
tincann <saabnl@gmail.com>
2015-01-02 21:07:35 +0100

    Removed scar code

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var tgapi = require('./lib/telegram-api.js');
 
 tgapi.connect(function(connection){
-    // setInterval(function(){
-    //     tgapi.cli.writeLine('msg Mies ' + +new Date());
-    // }.bind(this), 5000);
     connection.on('message', function(message){
         console.log('message:', message);
     });
