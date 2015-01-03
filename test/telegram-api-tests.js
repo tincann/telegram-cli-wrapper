94fe50c368c8e5024084b60a228c31d43bf2413b
tincann <saabnl@gmail.com>
2015-01-03 18:21:51 +0100

    Fixed APIConnection bug, telegram api tests

--- test/telegram-api-tests.js
+++ test/telegram-api-tests.js
@@ -1,10 +1,34 @@
 var assert = require('assert'),
-    telegram = require('../lib/telegram-api.js');
+    TelegramAPI = require('../lib/telegram-api.js');
 
-describe.skip('telegram-api', function() {
-    it('todo', function(){
-        telegram.connect(function(connection){
+describe('telegram-api', function() {
+    it('should open new connection', function(done){
+        var api = new TelegramAPI; 
+        api.connect(function(connection){
+            assert(connection);
+            done();
+        });
+    }); 
 
+    it('should emit error event from underlying socket', function(done){
+        var api = new TelegramAPI;
+        api.connect(function(connection){
+            var socket = connection.socket;
+            connection.on('error', function(){
+                done();
+            });
+            socket.emit('error');
+        });
+    });
+
+    it('should emit close event from underlying socket', function(done){
+        var api = new TelegramAPI;
+        api.connect(function(connection){
+            var socket = connection.socket;
+            connection.on('close', function(){
+                done();
+            });
+            socket.emit('close');
         });
     });
 });
