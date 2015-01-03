eb1aaae676728aa3eb8ad5e6c3b53f63ab871fc9
tincann <saabnl@gmail.com>
2015-01-03 17:54:52 +0100

    Telegram-cli-wrapper refactor

--- test/telegram-cli-wrapper-tests.js
+++ test/telegram-cli-wrapper-tests.js
@@ -1,16 +1,12 @@
 var assert = require('assert'),
-    tgcli = require('../lib/telegram-cli-wrapper.js');
+    TelegramCliWrapper = require('../lib/telegram-cli-wrapper.js');
 
 describe('telegram-cli-wrapper', function() {    
-
-    beforeEach(function(){
-        tgcli.stop();
-    });
-
     it('should not start the cli more than once', function(){
-        tgcli.start(function(socket){
+        var cli = new TelegramCliWrapper();
+        cli.start(function(socket){
             assert.throws(function(done){
-                tgcli.start(function(){
+                cli.start(function(){
                     done();
                 }); 
             }, /Process already started/);
@@ -18,14 +14,15 @@ describe('telegram-cli-wrapper', function() {
     });
 
     it('should correctly start and stop', function(done){
-        tgcli.start(function(socket){
-            tgcli.stop();
+        var cli = new TelegramCliWrapper();
+        cli.start(function(socket){
+            cli.stop();
             done();
         });
     });
 
     it('should handle already stopped cli', function(){
-        tgcli.stop();
-    })
-
+        var cli = new TelegramCliWrapper();
+        cli.stop();
+    });
 });
